0eb64d415c5f81095669a0f1a5bf4751b3580e15
naga <naga@ccit.com>
2025-03-10 12:57:48 +0530

    added files

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
+        // Pass the header text to the view
+        model.addAttribute("header", "Cloud Computing in Telugu");
+        return "index"; // Return the index.html template
+    }
+}
