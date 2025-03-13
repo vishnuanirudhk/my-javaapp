4eaea1ffd800b21eb041cc78bb1191e5b5f397ba
Naga Srinivasa <cloudcomputingchannel@gmail.com>
2025-03-13 21:33:29 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -17,7 +17,7 @@ public class App {
     @GetMapping("/")
     public String home(Model model) {
         // Pass the header text to the view
-        model.addAttribute("header", "CCIT");
+        model.addAttribute("header", "Cloud Computing in Telugu");
         return "index"; // Return the index.html template
     }
 }
