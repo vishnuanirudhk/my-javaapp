d4d0c7b689a7d5dab8f7b002f49c44203a36c3c1
vishnuanirudhk <vishnuanirudhk@gmail.com>
2025-06-04 22:25:45 -0300

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -17,7 +17,7 @@ public class App {
     @GetMapping("/")
     public String home(Model model) {
         // Pass the header text to the view
-        model.addAttribute("header", "Maven Deployment Pipleline");
+        model.addAttribute("header", "Maven Deployment Pipleline YAY !!");
         return "index"; // Return the index.html template
     }
 }
